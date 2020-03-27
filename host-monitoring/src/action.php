a8088a71a9d65a34e62de5dc434c83bfe7472852
UrBnW <40244829+UrBnW@users.noreply.github.com>
2020-03-27 19:09:19 +0100

    fix(UI): Keep current timepicker value

--- host-monitoring/src/action.php
+++ host-monitoring/src/action.php
@@ -239,7 +239,9 @@ jQuery(function() {
 
 	//initializing datepicker and timepicker
 	jQuery(".timepicker").each(function () {
-		$(this).val(moment().tz(localStorage.getItem('realTimezone') ? localStorage.getItem('realTimezone') : moment.tz.guess()).format("HH:mm"));
+		if (! $(this).val()) {
+			$(this).val(moment().tz(localStorage.getItem('realTimezone') ? localStorage.getItem('realTimezone') : moment.tz.guess()).format("HH:mm"));
+		}
 	});
 	jQuery("#start_time, #end_time").timepicker();
 	initDatepicker();
